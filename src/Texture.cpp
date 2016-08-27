8f8829e0281539489131288c368c404e15aaa399
Dario Ostuni <dario.ostuni@gmail.com>
2016-08-27 04:01:26 +0200

    Added MipMaps, hopefully this doesn't break too many things

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -13,9 +13,10 @@ Texture::Texture(shared_ptr<Logger> logger, uint32_t width, uint32_t height, con
     fpObjectLabel(GL_TEXTURE, texture_id, path_debug.size(), path_debug.data());
 #endif
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data.data());
+    glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glBindTexture(GL_TEXTURE_2D, 0);
 }
